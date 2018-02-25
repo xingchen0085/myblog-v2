5659e228d8b2cf0e76c1e018af9d0c08874991e4
xiao yang <474240165@qq.com>
2018-02-25 18:04:14 +0800

    Initial commit

---
+++ src/main/java/cn/myxinge/myblogv2/MyblogV2Application.java
@@ -0,0 +1,12 @@
+package cn.myxinge.myblogv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogV2Application.class, args);
+	}
+}
